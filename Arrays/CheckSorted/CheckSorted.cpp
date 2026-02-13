db46faf8c5a4d0fb6ef55d0245ba50d4abec0b20
Deep karmakar <159227454+deepkarmakar-dev@users.noreply.github.com>
2026-02-13 15:58:26 +0530

    Add CheckSorted.cpp to verify if an array is sorted

---
+++ Arrays/CheckSorted/CheckSorted.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+bool isSorted(int arr[], int n)
+{
+    for(int i = 0; i < n - 1; i++)
+    {
+        if(arr[i] > arr[i + 1])
+            return false;
+    }
+
+    return true;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    if(isSorted(arr, n))
+        cout << "Sorted";
+    else
+        cout << "Not Sorted";
+
+    return 0;
+}
