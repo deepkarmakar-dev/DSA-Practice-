a4344bcfded7f1dc7968b833d1e3769f3d1c5271
Deep karmakar <159227454+deepkarmakar-dev@users.noreply.github.com>
2026-02-13 15:38:29 +0530

    Implement right rotation of array by one position

---
+++ Arrays/RightRotate/RightRotate.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void rightRotateByOne(int arr[], int n)
+{
+    if (n <= 1)
+        return;
+
+    int temp = arr[n - 1];
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        arr[i + 1] = arr[i];
+    }
+
+    arr[0] = temp;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    rightRotateByOne(arr, n);
+
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+
+    return 0;
+}
