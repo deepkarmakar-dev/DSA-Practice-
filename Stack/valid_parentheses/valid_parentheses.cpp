04131b198e4b74f94a948cc52fc8d846241652dc
Deep karmakar <159227454+deepkarmakar-dev@users.noreply.github.com>
2026-02-25 20:41:23 +0530

    Create valid_parentheses.cpp

---
+++ Stack/valid_parentheses/valid_parentheses.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+bool isValid(string s)
+{
+    stack<char> st;
+
+    for(int i = 0; i < s.length(); i++)
+    {
+
+        char ch=s[i];
+
+
+        if(ch=='(' || ch=='[' || ch=='{')
+        {
+            st.push(ch);
+
+        }
+        else {
+        
+            if(st.empty())
+            {
+                return false;
+            }
+
+            if((ch == ')' && st.top() == '(') ||
+                    (ch == '}' && st.top() == '{') ||
+                    (ch == ']' && st.top() == '['))
+            {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+        }
+
+    }
+
+return st.empty();
+}
+
+int main() {
+
+    string s;
+
+    cout << "Enter parentheses string: ";
+    cin >> s;
+
+    if(isValid(s))
+        cout << "Valid" << endl;
+    else
+        cout << "Not Valid" << endl;
+
+    return 0;
+}
