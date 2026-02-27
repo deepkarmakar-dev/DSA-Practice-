d2e62cd102873bb0451cc27dd5f9e0dc0fd3ab56
Deep karmakar <159227454+deepkarmakar-dev@users.noreply.github.com>
2026-02-27 15:41:11 +0530

    Create longest-palindrome.cpp

---
+++ String/longest-palindrome/longest-palindrome.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <string>
+#include<vector>
+using namespace std;
+
+
+int longestPalindrome(string s)
+{
+   vector<int>upper(26,0);
+   vector<int>lower(26,0);
+    for(int i=0;i<s.length();i++)
+    {
+      if(s[i] >= 'a' && s[i] <= 'z')
+      {
+         lower[s[i]-'a']++;
+      }
+      else if(s[i] >= 'A' && s[i] <= 'Z'){
+        
+        upper[s[i]-'A']++;
+      }
+    }
+    int count=0;
+    bool odd=0;
+    for(int i=0;i<26;i++)
+    {
+       if(lower[i]%2==0)
+       {
+         count+=lower[i];
+       }
+       else{
+         count+=lower[i]-1;
+         odd=1;
+       }
+       
+       if(upper[i]%2==0)
+       {
+         count+=upper[i];
+       }
+       else{
+         count+=upper[i]-1;
+         odd=1;
+       }
+    }
+    return count+odd;
+}
+
+int main()
+{
+    string s = "babad";
+
+    int result = longestPalindrome(s);
+
+    cout << "Longest Palindrom" << result << endl;
+
+    return 0;
+}
