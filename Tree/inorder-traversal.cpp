033d284d9a5df8491f7f146f889e075af210f5e2
Deep karmakar <159227454+deepkarmakar-dev@users.noreply.github.com>
2026-03-03 09:02:08 +0530

    Create inorder-traversal.cpp

---
+++ Tree/inorder-traversal.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class node{
+public:
+    int data;
+    node* left;
+    node* right;
+    
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+ void inorder(node * root)
+{
+   
+   if(root==NULL)
+   return;
+   
+   inorder(root->left);
+   cout<<root->data<<" ";
+   inorder(root->right);
+}
+
+int main()
+{
+    node* root = new node(10);
+
+    root->left = new node(5);
+    root->right = new node(20);
+
+    root->left->left = new node(3);
+    root->left->right = new node(7);
+
+     inorder(root);
+    return 0;
+}
